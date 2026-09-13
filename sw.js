2df41f6b9dccd1b928187160b1fbd5aab80d521c
radarbrasilfestivais <marconiz.suporte@gmail.com>
2026-09-13 11:41:48 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -8,13 +8,12 @@
  * bug, etc.), basta mudar este número (ex: "v1" -> "v2") para forçar
  * todo mundo a baixar os arquivos novos automaticamente.
  */
-const CACHE_VERSION = "v7";
+const CACHE_VERSION = "v8";
 const CACHE_NAME = "gira-" + CACHE_VERSION;
 
 const ASSETS = [
   "./index.html",
   "./styles.css",
-  "./logo-icon.png",
   "./app.js",
   "./data.js",
   "./firebase-config.js",
